5770477f02a041933a3c1588d0a5b63b3bd790a8
ArnoldPires <arnold.pires92@gmail.com>
2020-11-26 18:09:57 -0500

    made a slideshow and navbar

--- class08-materials/hair-salon/js/main.js
+++ class08-materials/hair-salon/js/main.js
@@ -1,1 +1,14 @@
-
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("carouselImage");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
