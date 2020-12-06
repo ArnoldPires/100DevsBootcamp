678512c1994d2f6aa1a6738bafbbd7bc7715c7f8
ArnoldPires <arnold.pires92@gmail.com>
2020-12-06 17:19:55 -0500

    added text to angry parent and more code to the background picker files

---
+++ class09-materials/JavaScript-Crash-Course/js/main.js
@@ -0,0 +1,16 @@
+alert('Hello world')
+console.log('Hello world')
+// String, Numbers, Boolean, Null, undefined
+const name = 'John'; //String
+const age = 30; //Number
+const isCool = true; //Boolean
+const rating = 4.5; //Number
+const x = null; //Null
+const y = undefined; //Undefined
+let z;
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isCool)
+console.log(typeof rating)
+console.log(typeof x)
+console.log(typeof y)
