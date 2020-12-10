837d70f6380d0521befce8d434e820bc26fbef99
ArnoldPires <arnold.pires92@gmail.com>
2020-12-10 02:54:02 -0500

    some code

--- class10-materials/angry-parent-simulator/js/main.js
+++ class10-materials/angry-parent-simulator/js/main.js
@@ -1,74 +1,15 @@
-//This is a small text adventure
-//You are a bounty hunter going after your next target
-//You and your robot friend Cp-001 are on hot presuit of your target
-//This text adventure goes over your plans to capture or kill the target before it happens
-
-//The start:
-//Enter your bounty hunter name:
-//Cp-001 says your name and welcomes you back to the ship and introduces himself
-//You hit continue
-//Cp-001 tells you who the target is and presents you with a few different choices
-//On how best to capture the target
-//Choices:
-//Speed chase
-//Guns a blazing
-//Blackmail:
-//Speed chase: Cp-001 preps the ship with speed boosters and a tracking beacon
-//Guns a blazing: Cp-001 hands you some blasters, rockets, and thermal detonators
-//Blackmail: Cp-001 finds out the target has a family and threatens the targets family
-//in order to bring the target in
-//Speed chase ending: The target escapes in the speed chase
-//Guns a blazing: The target is killed during the firefight
-//Blackmail: The target believes the blackmail and surrenders
-
-//The code:
-//A button to activate the game
-//Cp-001 says your name and welcomes you
-//A continue button
-//Cp-001 tells you about the target and asks you to pick the three choices
-//User selects one of the choices
-//Cp-001 tells you the ending result of the choice you made
-//You are asked if you'd like to play again
-
-
-//What cp-001 will say:
-//Intro
-//Hello (your name here) I am Cp-001 and I am your helpful bounty droid.
-//Please select the continue button to continue with our bounty target
-//Our target is a notorious thug whose murdered several people in many different sectors of space
-//Are orders are to bring him in, dead or alive
-//Hit the continue button
-//I have laid out 3 different possible choices to accomplish our mission
-//Select the choice you want
-//Speed chase:
-//I will begin preping the ship with a tractor beam and speed boosting capibilities
-//Hit continue
-//CP-001 says: Unfortunately, during the chase, the target escaped would you like to play again?
-//Guns a blazing:
-//I have fully stocked our armory with all the latest blasters, rockets, and thermal detonators.
-//Lets blow some shit up!
-//Hit continue
-//We got the target, but he died during the firefight.However, we will still get rewarded. Would you like to play again?
-//Blackmail:
-//I've found the targets family. I can threaten them to get our target to turn himself in or else.
-//Hit continue
-//Success
-//The target will cooperate with us and turn himself in to us for capture. Will you like to play again?
-
-//Enter name/ Droid welcomes you
 const synth = window.speechSynthesis;
 document.querySelector('#welcome').addEventListener('click', run)
-
- function run() {
-   const fName = document.querySelector('#firstName').value
-   const lName = document.querySelector('#lastName').value
-   const welcomeText =  `Welcome, ${fName} ${lName}. I am Cp-001. I am your helpful and loyal combat droid companion and I am reporting for Duty. `
-   document.querySelector('#placeToWelcome').innerText = welcomeText
-   let welcomeThis = new SpeechSynthesisUtterance(welcomeText);
-   synth.speak(welcomeThis);
+function run() {
+  const fName = document.querySelector('#firstName').value
+  const lName = document.querySelector('#lastName').value
+  const welcomeText =  `Welcome, ${fName} ${lName}. I am Cp-001. I am your helpful and loyal combat droid companion and I am reporting for Duty. `
+  document.querySelector('#placeToWelcome').innerText = welcomeText
+  let welcomeThis = new SpeechSynthesisUtterance(welcomeText);
+  synth.speak(welcomeThis);
  }
-var utterThis = new SpeechSynthesisUtterance('#text');
-const textElement = document.getElementById('text')
+
+ const textElement = document.getElementById('text')
 const optionButtonsElement = document.getElementById('option-buttons')
 
 let state = {}
@@ -141,11 +82,11 @@ const textNodes = [
       },
       {
         text: 'Guns a Blazing',
-        nextText: 51
+        nextText: 6
       },
       {
         text: 'Blackmail',
-        nextText: 6
+        nextText: 7
       }
     ]
   },
@@ -170,8 +111,18 @@ const textNodes = [
     ]
   },
   {
-    id: 51,
+    id: 6,
     text: 'I have fully stocked our armory with all the latest blasters, rockets, and thermal detonators. Its time to blow shit up!.',
+    options: [
+      {
+        text: 'Continue',
+        nextText: 8
+      }
+    ]
+  },
+  {
+    id: 7,
+    text: 'Ive found the targets family.  With your permission, I can threaten them to get our target to turn himself in or else..',
     options: [
       {
         text: 'Continue',
@@ -180,17 +131,7 @@ const textNodes = [
     ]
   },
   {
-    id: 6,
-    text: 'Ive found the targets family.  With your permission, I can threaten them to get our target to turn himself in or else..',
-    options: [
-      {
-        text: 'Continue',
-        nextText: 7
-      }
-    ]
-  },
-  {
-    id: 9,
+    id: 8,
     text: 'We got the target, but he died during the firefight. We will still however, be rewarded. Would you like to play again?.',
     options: [
       {
@@ -200,7 +141,7 @@ const textNodes = [
     ]
   },
   {
-    id: 7,
+    id: 9,
     text: 'Success! The target will cooperate with us and turn himself in to us for capture. Will you like to play again?',
     options: [
       {
@@ -211,4 +152,3 @@ const textNodes = [
   }
 ]
 startGame()
-
--- class10-materials/template/js/main.js
+++ class10-materials/template/js/main.js
@@ -1,30 +1,69 @@
-var synth = window.speechSynthesis;
-var inputForm = document.querySelector('form');
-var inputTxt = document.querySelector('.txt');
-
-function speak(){
-    if (synth.speaking) {
-        console.error('speechSynthesis.speaking');
-        return;
-    }
-    if (inputTxt.value !== '') {
-    var utterThis = new SpeechSynthesisUtterance(inputTxt.value);
-    utterThis.onend = function (event) {
-        console.log('SpeechSynthesisUtterance.onend');
-    }
-    utterThis.onerror = function (event) {
-        console.error('SpeechSynthesisUtterance.onerror');
-    }
-    synth.speak(utterThis);
-  }
+var count = 0;
+function appear(){
+	items = document.getElementsByClassName("block");
+    if(count < items.length)
+       	items[count++].style.display = "";
 }
 
-inputForm.onsubmit = function(event) {
-  event.preventDefault();
-  speak();
-  inputTxt.blur();
+var count = 0;
+function disappear(){
+	items = document.getElementsByClassName("block");
+    if(count < items.length)
+       	items[count++].style.display = "block";
 }
 
+const synth = window.speechSynthesis;
+document.querySelector('#welcome').addEventListener('click', run)
 
+ function run() {
+   const fName = document.querySelector('#firstName').value
+   const lName = document.querySelector('#lastName').value
+   const welcomeText =  `Welcome, ${fName} ${lName}. I am Cp-001. I am your helpful and loyal combat droid companion and I am reporting for Duty. `
+   document.querySelector('#placeToWelcome').innerText = welcomeText
+   let welcomeThis = new SpeechSynthesisUtterance(welcomeText);
+   synth.speak(welcomeThis);
+ }
 
+document.querySelector('#talkOne').addEventListener('click', speak)
 
+function speak (message) {
+  const textOne = `hi`
+  var msg = new SpeechSynthesisUtterance(textOne)
+  document.querySelector('#text').innerText = textOne
+  var voices = window.speechSynthesis.getVoices()
+  msg.voice = voices[0]
+  window.speechSynthesis.speak(msg)
+}
+
+document.querySelector('#talkTwo').addEventListener('click', speakTwo)
+
+function speakTwo (message) {
+  const textTwo = `hi there`
+  var msg = new SpeechSynthesisUtterance(textTwo)
+  document.querySelector('#text').innerText = textTwo
+  var voices = window.speechSynthesis.getVoices()
+  msg.voice = voices[0]
+  window.speechSynthesis.speak(msg)
+}
+
+document.querySelector('#talkThree').addEventListener('click', speakThree)
+
+function speakThree (message) {
+  const textThree = `hi there again`
+  var msg = new SpeechSynthesisUtterance(textThree)
+  document.querySelector('#text').innerText = textThree
+  var voices = window.speechSynthesis.getVoices()
+  msg.voice = voices[0]
+  window.speechSynthesis.speak(msg)
+}
+
+document.querySelector('#talkFour').addEventListener('click', speakFour)
+
+function speakFour (message) {
+  const textFour = `hi there again butt face`
+  var msg = new SpeechSynthesisUtterance(textFour)
+  document.querySelector('#text').innerText = textFour
+  var voices = window.speechSynthesis.getVoices()
+  msg.voice = voices[0]
+  window.speechSynthesis.speak(msg)
+}
