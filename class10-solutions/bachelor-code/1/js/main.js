122b558caf2f3d7f1b2cd25411a00fc5ed82e06c
ArnoldPires <arnold.pires92@gmail.com>
2020-12-10 21:02:34 -0500

    removed a folder

--- class10-solutions/bachelor-code/1/js/main.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('#finalRose').addEventListener('click', hide)
-
-function hide(){
-	document.querySelector('#claire').style.display = 'none'
-	document.querySelector('#sharleen').style.display = 'none'
-}
