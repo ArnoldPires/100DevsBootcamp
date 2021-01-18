243c87ec9f2641e2474ab703827721a1fd4f3770
ArnoldPires <arnold.pires92@gmail.com>
2021-01-18 16:45:16 -0500

    went over most of the materials

--- class15-materials/arrays-fight-to-the-finish/js/main.js
+++ class15-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,7 +1,22 @@
-//Create an array of movie titles. Loop through the array and each element to the h2.
-
-
+//Create an array of movie titles. Loop through the array and add each element to the h2. 
+let movieTitles = [' ToyStory  ', ' LordOfTheRings  ', '  StarWars '] 
+movieTitles.forEach((item, i) => {
+    document.querySelector('h2').innerText += item
+})
+console.log(movieTitles)
+//OR ^ They are both the same thing
+for(let f = 0; f < movieTitles.length; f++) {
+    document.querySelector('h2').innerText += movieTitles[f]
+}
 //Create an array of numbers. Loop through the array and three to each number and replace the old number.
-
-
-//Find the average of all the numbers from question three
+let nums = [1,2,3]
+nums.forEach((itemTwo, g) => {
+    nums[g] = itemTwo + 3
+});
+console.log(nums)
+//Find the average of all the numbers from question two
+let sum = 0
+for(let h = 0; h<nums.length;h++) {
+    sum += nums[h]
+}
+console.log(sum / nums.length)
