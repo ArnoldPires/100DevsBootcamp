64c0034e903d1ec200d186a116137f3a59d295c5
ArnoldPires <arnold.pires92@gmail.com>
2021-01-22 17:44:16 -0500

    viewed class15

--- class16-materials/review-summer-games/js/main.js
+++ class16-materials/review-summer-games/js/main.js
@@ -1,1 +1,25 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+function takeArray(n1,n2,n3,n4,n5) {
+    alert((n1*n2*n3*n4*n5))
+}
+takeArray(1,2,3,4,5)
+
+//OR
+function multiplyArray(array) {
+    let product = 1
+    array.forEach((item, i) => {
+        product *= item
+    })
+    console.log(product)
+}
+multiplyArray([1,2,3,4,5])
+
+//OR
+function multiarray(arr) {
+    let productTwo = 1
+    for(let index = 0;index < arr.length; index++) {
+        productTwo = productTwo * arr[index]
+    }
+    console.log(productTwo);
+}
+multiarray([1,2,3,4,5])
