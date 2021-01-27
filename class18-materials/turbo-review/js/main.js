540e1d0de0f617a893a70378621ec39d1910871f
ArnoldPires <arnold.pires92@gmail.com>
2021-01-26 21:36:14 -0500

    class18 nasa

--- class18-materials/turbo-review/js/main.js
+++ class18-materials/turbo-review/js/main.js
@@ -12,34 +12,46 @@ if(multiword.search("apple") !== -1) {
 }
 // *Functions*
 // Create a function that returns rock, paper, or scissors as randomly as possible
-
- function rockPaperScissors() {
-     let ccp= Math.random();
-     if (ccp < 0.33) {
-       document.querySelector("#rpsAnswer").innerHTML = "Rock";
-    } else if (ccp < 0.66) {
-      document.querySelector("#rpsAnswer").innerHTML = "Paper";
-    } else {
-       document.querySelector("#rpsAnswer").innerHTML = "Scissors";
-    }
- }
+function rockPaperScissors() {
+  let random = Math.random();
+  if (random < 0.33) {
+    document.querySelector("#rpsAnswer").innerHTML = "Rock";
+    return "rock";
+  } else if (random < 0.66) {
+    document.querySelector("#rpsAnswer").innerHTML = "Paper";
+    return "paper";
+  } else {
+    document.querySelector("#rpsAnswer").innerHTML = "Scissors";
+    return "scissors";
+  }
+  
+}
 // // *Conditionals*
 // //Create a function that takes in a choice (rock, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
- function checkWin (choice) {
-     let botChoice = rockPaperScissors()
-     if((choice === "rock" && botChoice === "scissors") || (choice === "paper" && botChoice === "rock") || (choice === "scissors" && botChoice === "paper")) {
-         console.log("You Win!")
-         document.querySelector("#wtl").innerHTML = "You Win!";
-     } else if(choice === botChoice) {
-         console.log("You Tied!")
-         document.querySelector("#wtl").innerHTML = "You Tied!";
-     }else {
-         console.log("You Lost!")
-         document.querySelector("#wtl").innerHTML = "You Lost!";
-     }
- }
-  checkWin('rock')
- checkWin("paper");
-  checkWin("scissors");
+function checkWin(choice) {
+  let botChoice = rockPaperScissors();
+  if (
+    (choice === "rock" && botChoice === "scissors") ||
+    (choice === "paper" && botChoice === "rock") ||
+    (choice === "scissors" && botChoice === "paper")
+  ) {
+     document.querySelector("#wtl").innerHTML = "you won";
+  } else if (choice === botChoice) {
+    document.querySelector("#wtl").innerHTML = "you tied";
+  } else {
+    document.querySelector("#wtl").innerHTML = "you lost";
+  }
+}
+
 //*Loops*
 //Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
+function playGame(arr) {
+  arr.forEach((choice) => {
+    checkWin(choice);
+  });
+}
+playGame(["rock", "paper", "paper"]);
+
+function refreshPage() {
+  window.location.reload();
+} 
