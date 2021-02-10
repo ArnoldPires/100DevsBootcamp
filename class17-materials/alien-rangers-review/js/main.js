b8b9da4aa28e4fc5f807ce1477e368b7ca3a11d1
ArnoldPires <arnold.pires92@gmail.com>
2021-02-09 19:16:14 -0500

    pushing some code

--- class17-materials/alien-rangers-review/js/main.js
+++ class17-materials/alien-rangers-review/js/main.js
@@ -1,9 +1,9 @@
 //Arrays
-
 //Create and array of tv shows. Loop through and print each show to the console
-
+let tvShows = ['breakingBad', 'starWars', 'lordOfTheRings']
+tvShows.forEach(shows => console.log(shows))
 //Create and array of numbers
 //Return a new array of numbers that includes every even number from the previous Arrays
-
+let nums = [1,2,3,4,5,6,7,8,9,10]
 //Create a function that takes in an array of numbers
 //Alert the sum of the second lowest and the second highest number
