e0d45913e8c8e7375f0d2fdbdf9ae102b2ef75ca
ArnoldPires <arnold.pires92@gmail.com>
2021-02-12 15:07:12 -0500

    class17

--- class17-materials/alien-rangers-review/js/main.js
+++ class17-materials/alien-rangers-review/js/main.js
@@ -5,5 +5,13 @@ tvShows.forEach(shows => console.log(shows))
 //Create and array of numbers
 //Return a new array of numbers that includes every even number from the previous Arrays
 let nums = [1,2,3,4,5,6,7,8,9,10]
+function evens(arr) {
+    return arr.filter(num => num % 2 === 0)
+}
+console.log(evens(nums))
+//OR
+let numsTwo = [1,2,3,4,5,6,7,8,9,10]
+let onlyEven = array => array.filter(num => num % 2 === 0)
+console.log(onlyEven(numsTwo))
 //Create a function that takes in an array of numbers
 //Alert the sum of the second lowest and the second highest number
