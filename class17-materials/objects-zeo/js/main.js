1423a72258737e6d75afc54379e65f509041e6fa
ArnoldPires <arnold.pires92@gmail.com>
2021-02-22 16:16:10 -0500

    class17

--- class17-materials/objects-zeo/js/main.js
+++ class17-materials/objects-zeo/js/main.js
@@ -1,1 +1,13 @@
 //Create a stopwatch object that has four properties and three methods
+
+let stopwatch = {}
+stopwatch.color = 'red'
+stopwatch.brand = 'Ap'
+stopwatch.shape = 'square'
+stopwatch.size = 'large'
+
+stopwatch.start = function(){ console.log('STARTING')}
+stopwatch.stop = function(){console.log('STOPING')}
+stopwatch.sayBrand = function() {
+    alert(stopwatch.brand)
+}
