089a4a2b56c9728092b7c3fa45085f84c4c574fa
ArnoldPires <arnold.pires92@gmail.com>
2021-03-14 23:06:04 -0400

    review not done

---
+++ super-review/variable-fun-time/js/main.js
@@ -0,0 +1,18 @@
+//--- Easy
+//create a variable and assign it a number
+let x = 123
+//minus 10 from that number
+result = x - 10
+//print that number to the console
+console.log(result)
+//--- Medium
+//create a variable that holds a value from the input
+let num = document.querySelector('#danceDanceRevolution').value
+//add 25 to that number
+let addNumToNum = num + 25
+//alert that number
+alert(addNumToNum)
+//--- Hard
+//create a variable that holds the h1
+
+//add an event listener to that element that console logs the sum of the two previous variables
