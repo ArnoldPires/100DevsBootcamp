2cfa3cb319efcd6ef8c474581f7ae8d6f0455b55
ArnoldPires <arnold.pires92@gmail.com>
2021-03-26 14:32:34 -0400

    rap api

--- class32-materials/rap-api/server.js
+++ class32-materials/rap-api/server.js
@@ -1,24 +1,42 @@
-const express = require('express')
-const app = express()
-const PORT = 8000
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = 8000;
 
-let savage = {
-  'age': 28,
-  'birthname': 'Shéyaa Bin Abraham-Joseph',
-  'birthLocation': 'London, England'
+app.use(cors());
+
+let rappers = {
+  "21 savage": {
+    age: 28,
+    birthName: "Shéyaa Bin Abraham-Joseph",
+    birthLocation: "London, England",
+  },
+  "chance the rapper": {
+    age: 27,
+    birthName: "Chancelor Jonathan Bennett",
+    birthLocation: "Chicago, Illinois",
+  },
+  unknown: {
+    age: 0,
+    birthName: "unknown",
+    birthLocation: "unknown",
+  },
 };
 
-app.use(cors())
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
+});
 
-app.get('/', (request, response) => {
-    response.sendFile(__dirname + '/index.html')
-})
+app.get("/api/rappers/:rapperName", (request, response) => {
+  const rapName = request.params.rapperName.toLowerCase();
+  console.log(rapName);
+  if (rappers[rapName]) {
+    response.json(rappers[rapName]);
+  } else {
+    response.json(rappers["unknown"]);
+  }
+});
 
-app.get('/api/savage', (request, response) => {
-    response.json(savage)
-})
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
