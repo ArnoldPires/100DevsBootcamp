2747d9155a5e4e5488b18e689900c7c3c1bfbad0
ArnoldPires <arnold.pires92@gmail.com>
2021-03-28 17:59:15 -0400

    setting up rapper api for testing

---
+++ class34-materials/rapper-express/server.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 8000
+
+let db,
+    dbConnectionStr = 'mongodb+srv://ArnaldoP:Q3otfUtry7rPnVig@cluster0.3kklu.mongodb.net/rap?retryWrites=true&w=majority',
+    dbName = 'rap'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
+  (client) => {
+    console.log(`Connected to ${dbName} Database`);
+    db = client.db(dbName);
+  }
+);
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  db.collection("rappers")
+    .find()
+    .toArray()
+    .then((data) => {
+      response.render("index.ejs", { info: data });
+    })
+    .catch((error) => console.error(error));
+});
+
+app.post("/addRapper", (request, response) => {
+  db.collection("rappers")
+    .insertOne(request.body)
+    .then((result) => {
+      console.log("Rapper Added");
+      response.redirect("/");
+    })
+    .catch((error) => console.error(error));
+});
+
+app.delete("/deleteRapper", (request, response) => {
+  db.collection("rappers")
+    .deleteOne({ stageName: request.body.stageNameS })
+    .then((result) => {
+      console.log("Rapper Deleted");
+      response.json("Rapper Deleted");
+    })
+    .catch((error) => console.error(error));
+});
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
