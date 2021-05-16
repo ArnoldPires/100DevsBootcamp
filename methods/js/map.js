123670fd9acb0b2a04488e79b6c1963c663439ff
ArnoldPires <arnold.pires92@gmail.com>
2021-05-16 13:40:31 -0400

    setup a little css/the structure for each page

---
+++ methods/js/map.js
@@ -0,0 +1,6 @@
+document.querySelector("#exampleOne").addEventListener("click", giveAnswerOne);
+let numbers = [2, 4, 6, 8, 10];
+function giveAnswerOne() {
+    document.querySelector('#answerOne').innerHTML = numbers.map[Math.sqrt(Math.round())]
+}
+
