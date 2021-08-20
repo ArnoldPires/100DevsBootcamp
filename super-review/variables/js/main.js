c53583b8a2338d8da01a7bf52fe98b3e3e673f92
ArnoldPires <arnold.pires92@gmail.com>
2021-08-19 20:35:55 -0400

    some more stuff

---
+++ super-review/variables/js/main.js
@@ -0,0 +1,8 @@
+let message = "Hello I am that data being stored in a variable. I am also called a string, because I am random text.";
+alert(message)
+
+// Tasks:
+let admin
+let name = "John"
+admin = name
+alert("Task One: " + admin)
